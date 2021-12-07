afd17fbcda27314479289d98338204ff2e24c54a
zenjen-11 <jen.nicholls@teksavvy.com>
2021-12-07 00:06:37 +0000

    minor tweaks.

--- express_server.js
+++ express_server.js
@@ -9,6 +9,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[givenShortURL].longURL,
+   };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
